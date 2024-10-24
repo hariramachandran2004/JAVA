10cd38d9b3b674b019a95dac06799f8d96c951b6
hariramachandran2004 <hariramachandrant@gmail.com>
2024-10-24 16:13:58 +0530

    welcome to FSD

---
+++ JAVA/src/day1/Demo.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		
+		a=s.nextInt();
+		b=s.nextInt();
+		Math.pow(a, b);
+		System.out.println("add of a="+a+" and b="+b+" is:"+c+"");
+	}
+
+}
